32881aacd7c79320f24f5da0fc40804f7c250b9e
Rohit Raj <rohitrajptn2209@gmail.com>
2026-06-11 14:13:24 +0530

    Week 4 day2: POST method and docs page working

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class JDINPUT(BaseModel):
+    jd_text:str
 
 @app.get("/")
 def home():
     return {'message':'FastAPI is running successfully'}
 
-        
+@app.post('/test')
+def end_point(jd:JDINPUT):
+    return {'received':jd.jd_text}
